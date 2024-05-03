bdddcf41988685c42fbe234ad51cb55aec0e3b23
saadProf <saadprof101@gmail.com>
2024-05-03 09:30:56 +0600

    playNextSong function added.

--- music-player/script.js
+++ music-player/script.js
@@ -113,3 +113,14 @@ const pauseSong = () => {
     playButton.classList.remove("playing");
     audio.pause();
 };
+
+const playNextSong = () => {
+    if (userData?.currentSong === null) {
+        playSong(userData?.songs[0].id);
+    } else {
+        const currentSongIndex = getCurrentSongIndex();
+        const nextSong = userData?.songs[currentSongIndex + 1];
+
+        playSong(nextSong.id);
+    }
+};
