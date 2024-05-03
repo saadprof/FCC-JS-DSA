2e111a567b1f22a4ed735e0e22700608e330b9b0
saadProf <saadprof101@gmail.com>
2024-05-03 09:33:15 +0600

    shuffle function added.

--- music-player/script.js
+++ music-player/script.js
@@ -134,3 +134,14 @@ const playPreviousSong = () => {
         playSong(previousSong.id);
     }
 };
+
+const shuffle = () => {
+    userData?.songs.sort(() => Math.random() - 0.5);
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+
+    renderSongs(userData?.songs);
+    pauseSong();
+    setPlayerDisplay();
+    setPlayButtonAccessibleText();
+};
