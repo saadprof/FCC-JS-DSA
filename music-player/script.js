55433669ea0d37c2a015a1bcb3f3ce558b3245fc
saadProf <saadprof101@gmail.com>
2024-05-03 09:35:25 +0600

    setPlayerDisplay function added.

--- music-player/script.js
+++ music-player/script.js
@@ -178,3 +178,13 @@ const deleteSong = (id) => {
         });
     }
 };
+
+const setPlayerDisplay = () => {
+    const playingSong = document.getElementById("player-song-title");
+    const songArtist = document.getElementById("player-song-artist");
+    const currentTitle = userData?.currentSong?.title;
+    const currentArtist = userData?.currentSong?.artist;
+
+    playingSong.textContent = currentTitle ? currentTitle : "";
+    songArtist.textContent = currentArtist ? currentArtist : "";
+};
