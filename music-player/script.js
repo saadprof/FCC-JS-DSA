b0ba108cf621ca231a9daf106d4bd79d394e25fa
saadProf <saadprof101@gmail.com>
2024-05-03 09:34:29 +0600

    deleteSong function added.

--- music-player/script.js
+++ music-player/script.js
@@ -145,3 +145,36 @@ const shuffle = () => {
     setPlayerDisplay();
     setPlayButtonAccessibleText();
 };
+
+const deleteSong = (id) => {
+    if (userData?.currentSong?.id === id) {
+        userData.currentSong = null;
+        userData.songCurrentTime = 0;
+
+        pauseSong();
+        setPlayerDisplay();
+    }
+
+    userData.songs = userData?.songs.filter((song) => song.id !== id);
+    renderSongs(userData?.songs);
+    highlightCurrentSong();
+    setPlayButtonAccessibleText();
+
+    if (userData?.songs.length === 0) {
+        const resetButton = document.createElement("button");
+        const resetText = document.createTextNode("Reset Playlist");
+
+        resetButton.id = "reset";
+        resetButton.ariaLabel = "Reset playlist";
+        resetButton.appendChild(resetText);
+        playlistSongs.appendChild(resetButton);
+
+        resetButton.addEventListener("click", () => {
+            userData.songs = [...allSongs];
+
+            renderSongs(sortSongs());
+            setPlayButtonAccessibleText();
+            resetButton.remove();
+        });
+    }
+};
