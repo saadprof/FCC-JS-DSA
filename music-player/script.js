15510f8863bd1166ecc3efb5819e76a07014c368
saadProf <saadprof101@gmail.com>
2024-05-03 09:31:45 +0600

    playPreviousSong function added.

--- music-player/script.js
+++ music-player/script.js
@@ -124,3 +124,13 @@ const playNextSong = () => {
         playSong(nextSong.id);
     }
 };
+
+const playPreviousSong = () => {
+    if (userData?.currentSong === null) return;
+    else {
+        const currentSongIndex = getCurrentSongIndex();
+        const previousSong = userData?.songs[currentSongIndex - 1];
+
+        playSong(previousSong.id);
+    }
+};
