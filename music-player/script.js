6f9873e88db5bef7ecb3f5c5b486be10f525113e
saadProf <saadprof101@gmail.com>
2024-05-03 10:22:36 +0600

    function for sorting songs via name added.

--- music-player/script.js
+++ music-player/script.js
@@ -264,3 +264,19 @@ audio.addEventListener("ended", () => {
         setPlayButtonAccessibleText();
     }
 });
+
+const sortSongs = () => {
+    userData?.songs.sort((a, b) => {
+        if (a.title < b.title) {
+            return -1;
+        }
+
+        if (a.title > b.title) {
+            return 1;
+        }
+
+        return 0;
+    });
+
+    return userData?.songs;
+};
