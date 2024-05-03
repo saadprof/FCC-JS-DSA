3f1323efa455c65d737d16afcbe5d725d1adaf3d
saadProf <saadprof101@gmail.com>
2024-05-03 10:21:39 +0600

    event listener added on audio to play the next song automatically

--- music-player/script.js
+++ music-player/script.js
@@ -244,8 +244,23 @@ playButton.addEventListener("click", () => {
     }
 });
 
-
-pauseButton.addEventListener("click",  pauseSong);
+pauseButton.addEventListener("click", pauseSong);
 nextButton.addEventListener("click", playNextSong);
 previousButton.addEventListener("click", playPreviousSong);
 shuffleButton.addEventListener("click", shuffle);
+
+audio.addEventListener("ended", () => {
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
+
+    if (nextSongExists) {
+        playNextSong();
+    } else {
+        userData.currentSong = null;
+        userData.songCurrentTime = 0;
+        pauseSong();
+        setPlayerDisplay();
+        highlightCurrentSong();
+        setPlayButtonAccessibleText();
+    }
+});
