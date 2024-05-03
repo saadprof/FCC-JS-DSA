99aa44871f55b7b3b846e9a92a7d5e147733914d
saadProf <saadprof101@gmail.com>
2024-05-03 09:28:09 +0600

    playSong function added.

--- music-player/script.js
+++ music-player/script.js
@@ -84,3 +84,25 @@ let userData = {
     currentSong: null,
     songCurrentTime: 0,
 };
+
+const playSong = (id) => {
+    const song = userData?.songs.find((song) => song.id === id);
+    audio.src = song.src;
+    audio.title = song.title;
+
+    if (
+        userData?.currentSong === null ||
+        userData?.currentSong.id !== song.id
+    ) {
+        audio.currentTime = 0;
+    } else {
+        audio.currentTime = userData?.songCurrentTime;
+    }
+    userData.currentSong = song;
+    playButton.classList.add("playing");
+
+    highlightCurrentSong();
+    setPlayerDisplay();
+    setPlayButtonAccessibleText();
+    audio.play();
+};
