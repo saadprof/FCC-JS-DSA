955a26ccd00b12466700ed0306958dc877565a3b
saadProf <saadprof101@gmail.com>
2024-05-07 17:40:48 +0600

    RegExp used to remove unwanted characters, input value reversed to match. checkPalindrome function will return boolean value.

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -2,10 +2,13 @@ const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const resultP = document.getElementById("resultP");
 
-
 const sampleStr = "___ R Ace C Ar ___";
 const checkPalindrome = (str) => {
-    console.log(str);
-}
+    // Regular expression to replace unwanted characters from string
+    const regex = str.toLowerCase().replace(/[\W_]/g, "");
+    const reversedStr = regex.split('').reverse().join('');
+    
+    return regex === reversedStr;
+};
 
-checkPalindrome(sampleStr)
+checkPalindrome(sampleStr);
