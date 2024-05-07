240f179494b5ec23caa3be7aeb5e97f55853d399
saadProf <saadprof101@gmail.com>
2024-05-07 18:14:58 +0600

    manipulateDOM fuction added to update html according to user input.

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -2,7 +2,6 @@ const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const resultP = document.getElementById("resultP");
 
-const sampleStr = "___ R Ace C Ar ___";
 const checkPalindrome = (str) => {
     // Regular expression to replace unwanted characters from string
     const regex = str.toLowerCase().replace(/[\W_]/g, "");
@@ -11,4 +10,7 @@ const checkPalindrome = (str) => {
     return regex === reversedStr;
 };
 
-checkPalindrome(sampleStr);
+const manipulateDOM = () =>{
+    const isPalindrome = checkPalindrome(textInput.ariaValueMax.trim());
+    console.log(isPalindrome);
+}
