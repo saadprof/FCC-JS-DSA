078dd8fae1dd99b7a2841c7a5e0ff9d61680e572
saadProf <saadprof101@gmail.com>
2024-05-07 16:09:35 +0600

    palindrome checker function added.

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -1,3 +1,11 @@
 const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const resultP = document.getElementById("resultP");
+
+
+const sampleStr = "___ R Ace C Ar ___";
+const checkPalindrome = (str) => {
+    console.log(str);
+}
+
+checkPalindrome(sampleStr)
