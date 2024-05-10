bacabbc19a066f158ddbc79d4cd4539a44da63b3
saadProf <saadprof101@gmail.com>
2024-05-10 09:45:31 +0600

    taskData stores data from LS or an empty arr to work with. currentTask is self-explanatory

--- todo-app/script.js
+++ todo-app/script.js
@@ -10,3 +10,7 @@ const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
+
+// taskData retrieve data from local storage or be an empty array.
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
+let currentTask = {};
