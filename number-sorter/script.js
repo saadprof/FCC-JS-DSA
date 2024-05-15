56333d908a53215cd3ddc820ca4dbfc157086275
saadProf <saadprof101@gmail.com>
2024-05-15 13:09:25 +0600

    bubbleSort function added that takes array and sort accending order using nested for loops and value exchange method using temp.

--- number-sorter/script.js
+++ number-sorter/script.js
@@ -9,9 +9,12 @@ const sortInputArray = (event) => {
 
     // sort() method convert array item into string and then sort then according to UTF-16 code.
     // Here the sort() method take 2 value as input, subtract a from b, then sort numbers in accending order.
-    const sortedValues = inputValues.sort((a, b) => {
-        return a - b;
-    });
+    // const sortedValues = inputValues.sort((a, b) => {
+    //     return a - b;
+    // });
+
+    // bubble sort is another way to sort.
+    const sortedValues = bubbleSort(inputValues);
 
     updateUI(sortedValues);
 };
@@ -21,6 +24,21 @@ const updateUI = (array = []) => {
         const outputValueNode = document.getElementById(`output-value-${i}`);
         outputValueNode.innerText = num;
     });
+};
+
+// bubbleSort is to to sort number in accending order.
+// It starts sorting from left, campares with adjacent element, shift the highest value to the right. repeat until sorted.
+const bubbleSort = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
 };
 
 
